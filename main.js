96f6188868cd2e307ee103e3a44041af74140131
Grasword <45191745+Grasword@users.noreply.github.com>
2019-01-15 18:51:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // use dolar sign to indicate that its a dom node
 var $mainInput = document.getElementById('mainInput');
 
+// Create
 function createTask(name) {
     var dateTime = new Date();
     dateTime = dateTime.toLocaleString('ru-RU', { timeZone: 'Europe/Kiev' });
@@ -10,15 +11,8 @@ function createTask(name) {
         completed: false
     }
 }
-// Check
-function check() {
-    var id = this.dataset.id;
-    var tasks = getTasks();
-    tasks[id].completed = !tasks[id].completed;
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    render();
-}
 
+// Get
 function getTasks() {
     var tasks = [];
     var tasks_storage = localStorage.getItem('tasks');
@@ -27,56 +21,65 @@ function getTasks() {
             tasks = JSON.parse(tasks_storage);
         } catch (error){
             console.log('tasks is not a valid JSON')
-        }
+        } // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
     }
     return tasks
 }
 
 // Add
 function addTask(event) {
-    if (event.key === "Enter" || event.type === 'click') {
+    if (event.key === "Enter" || event.type === 'click') {   // input on enter
         var taskName = $mainInput.value;
         var tasks = getTasks();
         tasks.unshift(createTask(taskName));
         localStorage.setItem('tasks', JSON.stringify(tasks));
-        $mainInput.value = '';
+        $mainInput.value = ''; // reset input value
         render();
-        // see https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-        // return false  // что бы не срабатывал 'click' дальше
+        // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+        // return false
     } else {
         return false
     }
-
 }
-
 
 // Remove
 function remove() {
-    var id = this.dataset.id;
-    var tasks = getTasks();
-    tasks.splice(id, 1);   // Изначально написал .slice и час не мог понять почему не работает. лол
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    render();
-    return false;
+    if (confirm("Are you sure about that?")) {      // кривой confirm, надо переделать
+        var id = this.dataset.id;
+        var tasks = getTasks();
+        tasks.splice(id, 1);   // Изначально написал .slice и час не мог понять почему не работает. лол
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        render();
+        return false;
+    }
 }
 
+// Check
+function check() {
+    var id = this.dataset.id;
+    var tasks = getTasks();
+    tasks[id].completed = !tasks[id].completed;  // switch true / false
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    render();
+}
 
 
 // Render
 function render() {
     var tasks = getTasks();
-    //Создаю список с тасками подставляя время + таск + check + delete
+    // 												time + task + check + delete
     var html = '<ul>';
     for(var i=0; i<tasks.length; i++) {
         // use ternary operator for shorter if statement
         var taskStatus = (tasks[i].completed) ? 'checked' : '';
         // replace with templete literal using backticks (``)
-        html += '<li class="'+ taskStatus +'">' + tasks[i].date + ' : '+ tasks[i].name + '<button class="check" data-id="' + i  + '">' + '<i class="fas fa-check"></i></button>' + '<button class="remove" data-id="' + i  + '">' + '<i class="fas fa-trash"></i></button></li>';
+        html += '<li class="'+ taskStatus +'">' + tasks[i].date + ' : '+ tasks[i].name +
+            '<button class="check" data-id="' + i  + '">' + '<i class="fas fa-check"></i></button>' +
+            '<button class="remove" data-id="' + i  + '">' + '<i class="fas fa-trash"></i></button></li>';
     }
     html += '</ul>';
-    // подставляю
     document.getElementById('myItemList').innerHTML = html;
-    // remove button
+    // remove buttons
     var removeButtons = document.getElementsByClassName('remove');
     for (var i=0; i < removeButtons.length; i++) {
         removeButtons[i].addEventListener('click', remove);
@@ -94,7 +97,7 @@ document.getElementById('mainInput').addEventListener('keydown', addTask);
 render();
 
 
-
+// Old shit
 
 // function addTask() {
 //     var li = document.createElement("li");
@@ -129,11 +132,6 @@ render();
 // var dateTime = date+" "+time;
 
 
-/*
-if (tasks.length > 4) {
-  tasks.pop()
-}
- */
 
 //------------------------------------------
 
@@ -161,28 +159,3 @@ if (tasks.length > 4) {
 // }
 // var ul = document.createElement('ul');
 // document.getElementById('myItemList').appendChild(ul);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
